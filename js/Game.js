3ab910820432ad794b1e388520b88aba8dbab148
Asma2442 <70644511+Asma2442@users.noreply.github.com>
2020-11-15 20:55:09 +0530

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -16,16 +16,10 @@ class Game {
   }
 
   async start(){
-    if(gameState === 0){
       player = new Player();
-      var playerCountRef = await database.ref('playerCount').once("value");
-      if(playerCountRef.exists()){
-        playerCount = playerCountRef.val();
-        player.getCount();
-      }
-      form = new Form()
+      player.getCount();
+      form = new Form();
       form.display();
-    }
   }
 
   play(){
